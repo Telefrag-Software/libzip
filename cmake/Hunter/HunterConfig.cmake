5032809b99b79a4013c19c11f731fc406b4560aa
Kevin Scroggins <nitro404@gmail.com>
2021-07-06 19:15:00 -0400

    LibZIP Hunterization

---
+++ cmake/Hunter/HunterConfig.cmake
@@ -0,0 +1,21 @@
+include_guard()
+
+#hunter_config(BZip2
+#	URL "https://github.com/Telefrag-Software/bzip2/archive/refs/heads/hunter-1.0.6.zip"
+#	VERSION "1.0.6"
+#	SHA1 "957a9a1e137840af0f75dc6798f349d15cbfdb38"
+#)
+
+#hunter_config(OpenSSL VERSION 1.1.1j)
+
+hunter_config(ZLIB
+	URL "https://github.com/Telefrag-Software/zlib/archive/refs/heads/hunter-1.2.12.1.zip"
+	VERSION "1.2.12.1"
+	SHA1 "b41c9126bf63dfa53fbb496e24dcceb30ddabee4"
+)
+
+#hunter_config(zstd
+#	URL "https://github.com/Telefrag-Software/zstd/archive/refs/heads/hunter-1.5.0.zip"
+#	VERSION "1.5.0"
+#	SHA1 "724c091ed2c6ae6e5219111a75d021c42d41b892"
+#)
